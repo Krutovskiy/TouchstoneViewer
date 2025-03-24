4fd99bb2f24b17107a219c54d1c14dfce2001e0e
leon <lkrutovskiy@ptsecurity.com>
2025-03-24 23:58:15 +0700

    remove build

--- build/Desktop-Debug/CMakeFiles/appTouchstoneViewer.dir/build/Desktop-Debug/.rcc/qrc_appTouchstoneViewer_raw_qml_0.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/appTouchstoneViewer.dir/build/Desktop-Debug/.rcc/qrc_appTouchstoneViewer_raw_qml_0.cpp.o: \
-  /home/leon/Projects/TouchstoneViewer/build/Desktop-Debug/.rcc/qrc_appTouchstoneViewer_raw_qml_0.cpp
--- build/Desktop-Debug/CMakeFiles/appTouchstoneViewer.dir/build/Desktop-Debug/.rcc/qrc_qmake_TouchstoneViewer.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/appTouchstoneViewer.dir/build/Desktop-Debug/.rcc/qrc_qmake_TouchstoneViewer.cpp.o: \
-  /home/leon/Projects/TouchstoneViewer/build/Desktop-Debug/.rcc/qrc_qmake_TouchstoneViewer.cpp
